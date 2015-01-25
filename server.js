d10937b0906d73e3832ec293e9983f3da7f05ab7
DavideDaniel <davidedaniel@gmail.com>
2015-01-25 17:31:37 -0500

    quick update

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ var express = require( 'express' );
 var ejs = require( 'ejs' );
 var bodyParser = require( 'body-parser' );
 var app = express();
+var levelup = require( "level" )
+var db = levelup( "./myDb", {
+	valueEncoding: "json"
+} )
+var userDB = [];
 
-var userDB = {};
+db.createReadStream()
+	.on( 'data', function ( data ) {
+
+		console.log( data.value )
+		userDB.push( data.value )
+	} )
+	.on( 'error', function ( err ) {
+		console.log( 'Oh my!', err )
+	} )
+
+.on( 'end', function () {
+	console.log( 'Stream closed' )
+} );
+
 var User = function ( name, email ) {
 	this.name = name;
 	this.email = email;
@@ -22,19 +40,6 @@ var searchArtist = function () {
 	xhr.send();
 };
 
-function traversify( object ) {
-	for ( key in object ) {
-		if ( typeof ( object[ key ] ) == "object" ) {
-			console.log( "Object " + key );
-		}
-
-		if ( object[ key ] !== null && typeof ( object[ key ] ) == "object" ) {
-			console.log( "---- " + key );
-			traversify( object[ key ] );
-		}
-	}
-}
-
 app.use( bodyParser.urlencoded( {
 	extended: true
 } ) );
@@ -49,11 +54,10 @@ app.post( '/login', function ( req, res ) {
 	var name = req.body.name;
 	var email = req.body.email;
 	var user = new User( name, email );
-	userDB[ user.name ] = user;
-	traversify( userDB )
+
+	db.put( user.name, user );
 	console.log( "login " + user );
 	console.log( "login " + userDB );
-	console.log( "login " + userDB.name );
 
 	res.render( 'prefs.ejs', {
 		name: name
@@ -61,29 +65,25 @@ app.post( '/login', function ( req, res ) {
 } );
 
 app.post( '/:name/prefs', function ( req, res ) {
-	
+
 	var name = req.params.name;
 	var genre1 = req.body.genre1;
 	var genre2 = req.body.genre2;
 	var artist1 = req.body.artist1;
 	var artist2 = req.body.artist2;
 	var artist3 = req.body.artist3;
-	// userDB.name.preferences.genres.push( genre1, genre2 )
-	// userDB.name.preferences.artists.push( artist1, artist2, artist3 )
-	traversify( userDB )
-	// console.log( "prefs " + userDB.name.preferences.genres );
-	// console.log( "prefs " + userDB.name.preferences.artists );
+	userDB.name.preferences.genres.push( genre1, genre2 )
+	userDB.name.preferences.artists.push( artist1, artist2, artist3 )
+	console.log( "prefs " + userDB.name.preferences.genres );
+	console.log( "prefs " + userDB.name.preferences.artists );
 
-	// console.log( "prefs " + userDB );
-	// console.log( "prefs " + userDB.name );
-
-	// var arrayOfgenres = userDB.name.preferences.genres
-	// var arrayOfartists = userDB.name.preferences.artists
-	// console.log( arrayOfartists );
-	// console.log( arrayOfgenres );
+	var arrayOfgenres = userDB.name.preferences.genres
+	var arrayOfartists = userDB.name.preferences.artists
+	console.log( arrayOfartists );
+	console.log( arrayOfgenres );
 	res.render( 'result.ejs', {
-		// arrayOfartists: arrayOfartists,
-		// arrayOfgenres: arrayOfgenres
+		arrayOfartists: arrayOfartists,
+		arrayOfgenres: arrayOfgenres
 	} );
 } );
 
